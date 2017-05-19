2749e4ae524f587900130c6978f5ab3dbb423406
Harman Grewal <simrangrewal587@gmail.com>
2017-05-19 23:54:58 +0530

    Init Commit

---
+++ app/app.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+
+// Load Foundation
+// require('style!css!foundation-sites/dist/foundation.min.css')
+
+$(document).foundation();
+//APP css
+require('style!css!sass!applicationStyles')
+
+
+ReactDOM.render(
+  <p>Boilerplate3 Project</p>,
+  document.getElementById('app')
+);
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+// Create our app
+var app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(function(req, res, next) {
+	// req.headers is for Heroku it Does'not Exist Locally
+	if (req.headers['x-forwarded-proto'] === 'https') {
+		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
+	}
+});
+
+app.use(express.static('public'));
+app.listen(port, function () {
+  console.log(`Express server is up on port ${port}`);
+});
