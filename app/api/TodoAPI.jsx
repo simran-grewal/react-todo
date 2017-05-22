b191569bca26c0e5da89f5336f585cce064e97ed
Harman Grewal <simrangrewal587@gmail.com>
2017-05-23 00:59:57 +0530

    Adding Filter Functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -21,5 +21,44 @@ module.exports = {
     } else{
         return [];
     }
+  },
+
+  filterTodos: function(todos, showCompleted, searchText){
+
+    var filteredTodos = todos;
+    //Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    //Filter By Search text
+
+
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      if(searchText.length > 0){
+          if((text).indexOf(searchText) > -1){
+            return todo;
+          }
+      } else{
+        return todo;
+      }
+    });
+
+
+
+    //Sort Todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if(!a.completed && b.completed){
+        return -1;
+      } else if(a.completed && !b.completed){
+        return 1;
+      } else{
+        return 0;
+      }
+    });
+
+
+    return filteredTodos;
   }
 };
