43e9e6e6f8a235133269f796d7e1d1e73fd431e1
Harman Grewal <simrangrewal587@gmail.com>
2017-06-05 12:53:18 +0530

    Add todo reducer

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,5 +1,7 @@
+var  uuid = require('node-uuid');
+var moment = require('moment');
+
 export var searchTextReducer = (state = '', action) => {
-  action.searchText = 'hheel';
   switch(action.type) {
     case 'SET_SEARCH_TEXT':
         return action.searchText;
@@ -7,12 +9,43 @@ export var searchTextReducer = (state = '', action) => {
   }
 };
 
-//show completed take state and action and default state is false
-// TOGGLE_SHOW_COMPLETED
+
 export var showCompletedReducer = (state = false, action) => {
   switch(action.type) {
     case 'TOGGLE_SHOW_COMPLETED':
           return !state;
+    default: return state;
+  }
+}
+
+export var todoReducer =  (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+          return [
+            ...state,
+            {
+              id: uuid(),
+              text: action.text,
+              completed: false,
+              createdAt: moment().unix(),
+              completedAt: undefined
+            }
+          ];
+    case 'TOGGLE_TODO':
+          return state.map((todo) => {
+            if(todo.id == action.id) {
+              var newCompleted = !todo.completed;
+              return {
+                ...todo,
+                completed: newCompleted,
+                completedAt: newCompleted?moment().unix():undefined
+              };
+            }
+          });
+
+
+
+
     default: return state;
   }
 }
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,6 +1,6 @@
 var expect = require('expect');
 var df = require('deep-freeze-strict');
-
+var moment = require('moment');
 var reducers = require('reducers');
 
 describe('Reducers', () => {
@@ -28,5 +28,39 @@ describe('Reducers', () => {
       });
 
 
+      it('Should Add todo item', () => {
+        var action = {
+          type: 'ADD_TODO',
+          text: 'Walk the dog'
+        };
+
+        var res = reducers.todoReducer(df([]), df(action));
+
+        expect(res.length).toEqual(1);
+        expect(res[0].text).toEqual(action.text);
+      });
+
+      it('Should Toggle Todo', () => {
+          var action = {
+            type: 'TOGGLE_TODO',
+            id: 1
+          }
+
+          var arr = [
+            {
+              id: 1,
+              text: 'simran',
+              completed: true,
+              createdAt: moment().unix(),
+              completedAt:1212
+            }
+          ];
+          var res = reducers.todoReducer(df(arr), df(action));
+          expect(res[0].id).toEqual(arr[0].id);
+          expect(res[0].completed).toEqual(false);
+          expect(res[0].completedAt).toEqual(undefined);
+
+
+      });
     });
 });
