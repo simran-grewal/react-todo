a717cd91b1c0e2f65310e67a7ba89e8bd94dd6cd
Harman Grewal <simrangrewal587@gmail.com>
2017-06-07 23:18:48 +0530

    Modifying webConfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 var webpack = require('webpack');
 var path = require('path');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
   entry: [
     'script!jquery/dist/jquery.min.js',
@@ -55,5 +58,5 @@ module.exports = {
       path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
 };
